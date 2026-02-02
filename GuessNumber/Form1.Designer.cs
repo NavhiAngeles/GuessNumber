4a47793baac20a7d2e66d7080911a7f34b91e7ca
NavhiAngeles <ivhanangeles.04@gmail.com>
2026-02-02 15:39:48 +0800

    Complete na to Ready na kay Sir

--- GuessNumber/Form1.Designer.cs
+++ GuessNumber/Form1.Designer.cs
@@ -29,7 +29,53 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            GuessBox = new TextBox();
+            AnswerBox = new TextBox();
+            enter = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveBorder;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(45, 218);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 45);
+            label1.TabIndex = 0;
+            label1.Text = "The answer is:";
+            // 
+            // GuessBox
+            // 
+            GuessBox.BackColor = SystemColors.InactiveCaption;
+            GuessBox.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            GuessBox.Location = new Point(70, 77);
+            GuessBox.Name = "GuessBox";
+            GuessBox.Size = new Size(182, 50);
+            GuessBox.TabIndex = 2;
+            GuessBox.TextChanged += GuessBox_TextChanged;
+            // 
+            // AnswerBox
+            // 
+            AnswerBox.BackColor = SystemColors.InactiveCaption;
+            AnswerBox.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            AnswerBox.Location = new Point(70, 282);
+            AnswerBox.Name = "AnswerBox";
+            AnswerBox.Size = new Size(182, 50);
+            AnswerBox.TabIndex = 3;
+            // 
+            // enter
+            // 
+            enter.BackColor = SystemColors.InactiveCaption;
+            enter.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            enter.Location = new Point(112, 133);
+            enter.Name = "enter";
+            enter.Size = new Size(97, 39);
+            enter.TabIndex = 4;
+            enter.Text = "ENTER";
+            enter.UseVisualStyleBackColor = false;
+            enter.Click += enter_Click;
             // 
             // Form1
             // 
@@ -38,11 +84,21 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(329, 420);
+            Controls.Add(enter);
+            Controls.Add(AnswerBox);
+            Controls.Add(GuessBox);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox GuessBox;
+        private TextBox AnswerBox;
+        private Button enter;
     }
 }
--- GuessNumber/Form1.cs
+++ GuessNumber/Form1.cs
@@ -1,10 +1,62 @@
-namespace GuessNumber
+﻿namespace GuessNumber
 {
     public partial class Form1 : Form
     {
+        private int randomNumber;
+
         public Form1()
         {
             InitializeComponent();
+            GenerateRandomNumber();
+        }
+
+        private void GenerateRandomNumber()
+        {
+            Random rnd = new Random();
+            randomNumber = rnd.Next(1, 10);
+        }
+
+        private void GuessBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void enter_Click(object sender, EventArgs e)
+        {
+            int userGuess;
+
+            if (!int.TryParse(GuessBox.Text, out userGuess))
+            {
+                MessageBox.Show("Please enter a valid number.");
+                return;
+            }
+
+            if (userGuess < 1 || userGuess > 10)
+            {
+                MessageBox.Show("Please guess a number between 1 and 10.");
+                return;
+            }
+
+            if (userGuess < randomNumber)
+            {
+                MessageBox.Show("Too low! Try again.");
+            }
+            else if (userGuess > randomNumber)
+            {
+                MessageBox.Show("Too high! Try again.");
+            }
+            else
+            {
+
+                AnswerBox.Text = randomNumber.ToString();
+                MessageBox.Show("Correct! You guessed the number 🎉");
+                
+
+                GuessBox.Clear();
+                AnswerBox.Clear();
+                GenerateRandomNumber();
+                GuessBox.Focus();
+            }
         }
     }
 }
